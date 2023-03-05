30e2b67b9e8e12b3932e350627957358a71f0496
Olivier BV <olivier.b.v@octanetechno.com>
2023-03-05 15:52:47 -0500

    Trying to block iframes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const helmet = require('helmet');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(helmet.frameguard({action:'deny'}));
 app.use(express.static(__dirname + "/dist/"));
 
 app.get(/.*/, function(req, res) {
